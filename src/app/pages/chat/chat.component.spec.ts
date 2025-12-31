988dd982e7084f70eb075dc2070ed8a6ca0a2e7a
HARITH <six6assassin@gmail.com>
2026-01-01 03:09:25 +0800

    ng-chat-ui-setup

--- src/app/pages/chat/chat.component.spec.ts
+++ src/app/pages/chat/chat.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatComponent } from './chat.component';
+import { beforeEach, describe, it } from 'node:test';
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
@@ -21,3 +22,7 @@ describe('ChatComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+function expect(component: ChatComponent) {
+  throw new Error('Function not implemented.');
+}
+
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://iyofwqfbewjkskxnamlc.supabase.co',
+  supabaseKey: 
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5b2Z3cWZiZXdqa3NreG5hbWxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMDM0NzgsImV4cCI6MjA4MjY3OTQ3OH0.1N3l5H-k4JsrXv9K78-AzU4E81-MMjrtGoARgQWgIHY',
+};
